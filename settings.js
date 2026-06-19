73efbecf2a6e588d017b15b60bec29488b5e2101
ravinu177 <ravinulakshan18@gmail.com>
2026-06-19 10:20:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,17 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
-
-module.exports = {
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94768223718' : process.env.OWNER_NUMBER,
-SESSION_ID: process.env.SESSION_ID === undefined ? 'KnightBot!eFlU0w7Iw0Y6G3M9_T4nKl1P2qR5sV7bN8mX0zC3xV6bB9n' : process.env.SESSION_ID,
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'ravinu177' : process.env.GITHUB_USER_NAME,
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN
+const settings = {
+  packname: 'Knight Bot',
+  author: '‎',
+  botName: "Knight Bot",
+  botOwner: 'Professor', // Your name
+  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
 };
+
+module.exports = settings;
