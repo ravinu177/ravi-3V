2569917299bc8003d69545810676ba9aac792d6b
ravinu177 <ravinulakshan18@gmail.com>
2026-06-19 10:17:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94768223718' : process.env.OWNER_NUMBER,
-SESSION_ID: process.env.SESSION_ID === undefined ? 'KnightBot!eFlU0w7I#w0Y6G3M9_T4nKl1P2qR5sV7bN8mX0zC3xV6bB9n' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'KnightBot!eFlU0w7Iw0Y6G3M9_T4nKl1P2qR5sV7bN8mX0zC3xV6bB9n' : process.env.SESSION_ID,
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'ravinu177' : process.env.GITHUB_USER_NAME,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN
 };
